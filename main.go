969597e79dbf0b27e346b106aa4df465791ce9a2
zakkiiy <131367248+zakkiiy@users.noreply.github.com>
2025-06-28 23:32:25 +0900

    entrypont

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+  router := gin.Default()
+  router.GET("/ping", func(c *gin.Context) {
+    c.JSON(200, gin.H{
+      "message": "pong",
+    })
+  })
+  router.Run() // 0.0.0.0:8080 でサーバーを立てます。
+}
